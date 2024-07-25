20133764750960bd4c1a92c3ad42acd775cc4c88
JoviOakz <joao.mateussdc@gmail.com>
2024-07-25 10:18:24 -0300

    feat: import components

--- AulaReact/src/pages/Home.jsx
+++ AulaReact/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import NavBar from "../components/NavBar";
+import PageContent from "../components/PageContent";
+import Footer from "../components/Footer";
 
 const Home = () => {
     return (
         <>
-            
+            <NavBar></NavBar>
+            <PageContent></PageContent>
+            <Footer></Footer>
         </>
     )
 }
