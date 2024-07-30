8a5f148977467354d4b67b4315a355675cf8e7d7
JoviOakz <joao.mateussdc@gmail.com>
2024-07-30 10:16:36 -0300

    feat: style added

--- AulaReact/src/pages/HomePage/Home.jsx
+++ AulaReact/src/pages/HomePage/Home.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../../components/NavBar";
 import PageContent from "../../components/PageContent";
 import Footer from "../../components/Footer";
+import './style.css'
 
 const Home = () => {
     return (
