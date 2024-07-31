daa1336747ca59ac6be026e945a8226e543f1ad3
JoviOakz <joao.mateussdc@gmail.com>
2024-07-31 10:32:42 -0300

    feat: updated css of loginPage and homePage

--- AulaReact/src/main.jsx
+++ AulaReact/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import Login from './pages/LoginPage/Login'
-import Home from './pages/HomePage/Home'
+import Login from './pages/LoginPage/Login'
+// import Home from './pages/HomePage/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Login/> */}
-    <Home/>
+    <Login/>
+    {/* <Home/> */}
   </React.StrictMode>,
 )
