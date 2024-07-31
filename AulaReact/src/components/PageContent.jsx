113034fe73fb1f794063894bb5200c0636ebd072
JoviOakz <joao.mateussdc@gmail.com>
2024-07-31 08:56:31 -0300

    feat: updated white to a better color

--- AulaReact/src/components/PageContent.jsx
+++ AulaReact/src/components/PageContent.jsx
@@ -10,27 +10,27 @@ const PageContent = () => {
         <div className='page-content'>
             <div className='card'>
                 <button onClick={ () => setVisibility(!visibility) }>BOTÃO</button>
-                {visibility && <h2>Faça um botão que altere a visibilidade de um texto dentro da página. visível ou escondido.</h2>}
+                {visibility && <h3>Faça um botão que altere a visibilidade de um texto dentro da página. visível ou escondido.</h3>}
             </div>
 
             <div className='card'>
                 <Clock/>
-                <h2>Exiba o horário atual, atualizado de segundo em segundo.</h2>
+                <h3>Exiba o horário atual, atualizado de segundo em segundo.</h3>
             </div>
 
             <div className='card'>
                 <button onClick={ () => setCounter(counter + 1) }>BOTÃO</button>
                 <h3>Você clicou {counter} vezes.</h3>
-                <h2>Exiba um contador de cliques ao centro da tela.</h2>
+                <h3>Exiba um contador de cliques ao centro da tela.</h3>
             </div>
 
             <div className='card' style={{ backgroundColor: color }}>
                 <button onClick={ () => setColor( color == '#F0F2F5' ? 'aquamarine' : '#F0F2F5' ) }>BOTÃO</button>
-                <h2>Faça um botão que altere a cor de fundo de uma div.</h2>
+                <h3>Faça um botão que altere a cor de fundo de uma div.</h3>
             </div>
 
             <div className='card'>
-                <h2>Faça um campo de texto que exiba uma mensagem de bem-vindo com o nome inserido, alterando o estado.</h2>
+                <h3>Faça um campo de texto que exiba uma mensagem de bem-vindo com o nome inserido, alterando o estado.</h3>
             </div>
         </div>
     )
