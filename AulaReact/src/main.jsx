524d20122b6d44352a27ed581b13c5e9d9a46888
JoviOakz <joao.mateussdc@gmail.com>
2024-08-02 08:41:33 -0300

    feat: tailwind home created

--- AulaReact/src/main.jsx
+++ AulaReact/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Login from './pages/LoginPage/Login'
-import Home from './pages/HomePage/Home'
-// import HomeTailwind from './pages/HomePageTailwind/HomeTailwind'
+// import Home from './pages/HomePage/Home'
+import HomeTailwind from './pages/HomePageTailwind/HomeTailwind'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Login/> */}
-    <Home/>
-    {/* <HomeTailwind/> */}
+    {/* <Home/> */}
+    <HomeTailwind/>
   </React.StrictMode>,
 )
