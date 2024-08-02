fe87bd270dbdded94a634c1c73d49b483ebaa2a8
JoviOakz <joao.mateussdc@gmail.com>
2024-08-02 08:55:34 -0300

    feat: att tailwind page

--- AulaReact/src/main.jsx
+++ AulaReact/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Login from './pages/LoginPage/Login'
 // import Home from './pages/HomePage/Home'
-import HomeTailwind from './pages/HomePageTailwind/HomeTailwind'
+import HomeTailwind from './pages/HomeTailwind'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
