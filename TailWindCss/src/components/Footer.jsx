65a4214ced8cceb6e7cdeacdd7b17d53d9212bea
JoviOakz <joao.mateussdc@gmail.com>
2024-08-02 10:14:08 -0300

    feat: footer css finished

--- TailWindCss/src/components/Footer.jsx
+++ TailWindCss/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div/>
+        <div className='footer h-14 w-full bg-teal-300 p-4 shadow-[0_-4px_6px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)]'/>
     )
 }
 
