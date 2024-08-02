858a0daed5ba551ff1a27398f075c10fb2a869d8
JoviOakz <joao.mateussdc@gmail.com>
2024-08-02 10:00:44 -0300

    feat: added shadow to navbar

--- TailWindCss/src/components/NavBar.jsx
+++ TailWindCss/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Logo from '/logo.jpg'
 
 const NavBar = () => {
     return (
-        <div className='navbar h-14 w-full bg-teal-300 flex justify-between p-4'>
+        <div className='navbar h-14 w-full bg-teal-300 flex justify-between p-4 shadow-md'>
             <div className='flex justify-between w-24'>
                 <img className='w-12 h-7 rounded-3xl' src={Logo}/>
                 Krovi
