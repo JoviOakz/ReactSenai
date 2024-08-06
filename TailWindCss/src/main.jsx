d399f5cc09565b6e80b9059670367faf3d917e51
JoviOakz <joao.mateussdc@gmail.com>
2024-08-06 08:50:15 -0300

    feat: working

--- TailWindCss/src/main.jsx
+++ TailWindCss/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './pages/Login'
-// import Home from './pages/Home'
+// import Login from './pages/Login'
+import Home from './pages/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
-    {/* <Home/> */}
+    {/* <Login/> */}
+    <Home/>
   </React.StrictMode>,
 )
