6c2fcaa6e731201025388347c1ca6c3d80e060cf
JoviOakz <joao.mateussdc@gmail.com>
2024-08-06 09:04:03 -0300

    feat: Input.jsx updated

--- TailWindCss/src/components/Input.jsx
+++ TailWindCss/src/components/Input.jsx
@@ -11,7 +11,7 @@ const Input = ({ type, placeholder, value, onChange }) => {
         className="border border-gray-300 rounded-md py-1 px-3 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
       />
     </div>
-  );
-};
+  )
+}
 
 export default Input;
