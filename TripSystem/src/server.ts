9d55f4ead5344f693e207b6b19e8acd34a73cfa9
JoviOakz <joao.mateussdc@gmail.com>
2024-08-16 11:00:13 -0300

    feat: confirm-trip working

--- TripSystem/src/server.ts
+++ TripSystem/src/server.ts
@@ -1,8 +1,8 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { create } from "domain";
 import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify();
 
@@ -14,6 +14,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333 }).then(() => {
     console.log("Server running")
